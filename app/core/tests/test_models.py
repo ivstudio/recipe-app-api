bec058443ae7b80ffeb28c4ebfbae159c98608d7
Iggy Villamar <iggy@ivstudio.com>
2021-02-19 11:29:55 -0500

    Add untracted tests

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test create a new user with an email is successful"""
+        email = 'test@test.com'
+        password = 'test12345'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new ser is normalized"""
+        email = 'test@TEST.com'
+        user = get_user_model().objects.create_user(email, 'test12345')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test for valid email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test create a new super user"""
+        user = get_user_model() \
+            .objects.create_superuser('test@test.com', 'test12345')
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
