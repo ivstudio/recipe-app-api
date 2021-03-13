49d0e803082861ee3b2c11041d6acb0eb07385fa
Iggy Villamar <iggy@ivstudio.com>
2021-03-13 08:51:57 -0500

    Add recipe image upload feature

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/user/', include('user.urls')),
-    path('api/recipe/', include('recipe.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('api/user/', include('user.urls')),
+                  path('api/recipe/', include('recipe.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
